ddb5cda572043d68dff2b4b6ea7112cdfaea7bef
Franklin Lima <franklin@pilhadigital.com.br>
2015-10-02 10:06:56 -0300

    DUMP 09:24 - 02/10/15

--- assets/js/verifica.js
+++ assets/js/verifica.js
@@ -1,24 +1,19 @@
 $(document).ready(function() {
-$("#verifica").click(function() {
-    
-    email = $("#email").val();
-    senha = $("#senha").val();
-    alert(email);
-    alert(senha);
-    
-    $.ajax({
-        type:  'post',
-        data:  'email='+email+'&senha='+senha,
-        url:   'http://localhost/testephp/welcome/verificar',
-        success: function(acesso){
-            //alert(acesso);
-             if(acesso==1){
-                window.location="http://localhost/testephp/welcome/painel";
-            }else{
-                alert('Acesso negado');
+    $("#verifica").click(function() {
+        email = $("#email").val();
+        senha = $("#senha").val();
+        $.ajax({
+            type:  'post',
+            data:  'email='+email+'&senha='+senha,
+            url:   'http://localhost/testephp/welcome/verificar',
+            success: function(acesso){
+                 if(acesso==1){
+                    window.location="http://localhost/testephp/welcome/painel";
+                }else{
+                    $("#negado").html('<h3>Acesso negado</h3>');
+                }
             }
-        }
-    });    
-});
+        });    
+    });
 });
         
